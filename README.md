25a60efd905f22a743ad603eb1ba9e2b7f293523
Jayoung Ryu <sehibb@gmail.com>
2018-11-26 18:24:22 +0900

    merged JayoungRyu

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ CS360 Team 11
 
 -조교님 제공 Javascript+HTML 예시 코드 (참고용) : https://github.com/SuminHan/CS360_tutorial  
 -mySQL 기본 환경 설치 (XAMPP 이용) : http://recoveryman.tistory.com/105
+
+파일 실행 방법 : 
+1. CS360-HOEP-Project 폴더를 C드라이브 바로 아래에 오도록 설정  
+2. mySQL에 root 계정으로 로그인 한 뒤, SOURCE C:/CS360-HOEP-Project/initial_db_construction.sql 명령 실행 (SQL 스크립트 실행) 
+3. CMD로 들어간 뒤, CS360-HOEP-Project 안에 있는 app_univ.js 실행 (node app_univ.js)
+4. Web상에서 localhost:8000 으로 들어가서, 원하는 명령 내려주면 결과를 볼 수 있음
--- app_univ.js
+++ app_univ.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
     port     : 3306,
     user     : 'tester',
     password : '1234',
-    database : 'cs360_tutorial',
+    database : 'cs360_hoep',
 });
 
 connection.connect(); // Connection to MySQL
@@ -21,9 +21,9 @@ app.use('/', express.static(__dirname + '/public')); // you may put public js, c
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
-})); 
+}));
 
-
+var queryStr = ""
 
 // "node app.js" running on port 3000
 app.listen(8000, function () {
@@ -38,17 +38,56 @@ app.get('/', function (req, res) {
 });
 
 
-
 // get action to give raw data for user_tbl: "http://localhost/listAPI"
 app.post('/inital_search', function (req, res) {
 	console.log(req.body); // log to the node.js server
-	
-	queryStr = 'select * from user_tbl where id = '
-		+ req.body.user_region[0] + ';';
-	
+
+	// [Work-to-do] "user_dept"와 "user_region"에 아무런 값을 입력하지 않는 경우 에러 띄우기
+	//window.alert("Please select more than one condition.")
+
+	// Construct queryStr
+	queryStr = 'SELECT Name, Univ_track FROM REQUEST1 WHERE DID = "'
+					+ req.body.user_dept + '" AND RID '
+
+	// Case 1. Multiple regions selected by user
+	if(req.body.user_region.length > 1){
+		var region_str_set = "("
+		var index = 1
+
+		for(index = 1; index < req.body.user_region.length; index++){
+			region_str_set = region_str_set.concat(req.body.user_region[index], ',')
+		}
+		// Replace last character ',' with ')' ()
+		// https://stackoverflow.com/questions/36630230/replace-last-character-of-string-using-javascript
+		var region_str_set = region_str_set.replace(/.$/,")")
+
+		queryStr = queryStr.concat("IN ", region_str_set, ";")
+
+	// Case 2. Only one region selected by user
+	}else{
+		queryStr = queryStr.concat('= ', req.body.user_region[0], ';')
+	}
+
+	/*
+	queryStr = 'SELECT Name, Univ_track FROM REQUEST1 WHERE RID = "'
+		+ req.body.user_region[0]
+		+ '" AND DID = "'
+		+ req.body.user_dept
+		+ '";';
+	*/
+	/*
 	connection.query(queryStr, function (err, rows) {
 		if (err) throw err;
 		console.log(rows);
 		res.send(rows);
 	})
-});	
+	*/
+	res.sendFile(__dirname + "/result.html")
+});
+
+app.get('/listAPI', function (req, res) {
+	connection.query(queryStr, function (err, rows) {
+		if (err) throw err;
+		res.send(rows);
+	})
+});
